d4213fa9cf770f609dd63fb6f3ffe3ba1f08e8c1
soszin <d.sroczynski92@gmail.com>
2019-04-02 23:04:41 +0200

    remove unused import statement

--- teams.py
+++ teams.py
@@ -1,5 +1,4 @@
 from scrapers.Team import Team
-from datetime import datetime
 
 ids = [6665, 5973, 4869, 4608, 6667, 4411, 6211, 9215, 9565, 7533, 6673, 4991, 5995, 8120, 8513, 7801, 8481, 7532, 8297, 8068]
 
