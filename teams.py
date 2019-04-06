647925f30492ac85b85a05870252c1b1adbd256d
soszin <d.sroczynski92@gmail.com>
2019-04-06 15:05:23 +0200

    add outputs directory

--- teams.py
+++ teams.py
@@ -3,7 +3,7 @@ import csv
 
 ids = [6665, 5973, 4869, 4608, 6667, 4411, 6211, 9215, 9565, 7533, 6673, 4991, 5995, 8120, 8513, 7801, 8481, 7532, 8297, 8068]
 
-with open('teams.csv', mode='w') as team:
+with open('outputs/teams.csv', mode='w') as team:
     team_writer = csv.writer(team, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     team_writer.writerow([
         'Name', 'Country', 'World ranking', 'Flag', 'Logo'
