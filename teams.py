51a30fd916792331ed0fc2ce276d7a2066192823
soszin <d.sroczynski92@gmail.com>
2019-04-06 15:03:38 +0200

    add csv headers

--- teams.py
+++ teams.py
@@ -5,7 +5,9 @@ ids = [6665, 5973, 4869, 4608, 6667, 4411, 6211, 9215, 9565, 7533, 6673, 4991, 5
 
 with open('teams.csv', mode='w') as team:
     team_writer = csv.writer(team, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-
+    team_writer.writerow([
+        'Name', 'Country', 'World ranking', 'Flag', 'Logo'
+    ])
     for id in ids:
         team = Team(id)
         team_writer.writerow([
